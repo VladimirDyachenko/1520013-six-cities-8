8e3b7a28588cab3591acea4d8649f048139161b7
VladimirDyachenko <d.vovan3@gmail.com>
2021-10-05 14:51:28 +0300

    Описал все маршруты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AppRoute } from '../../utils/const';
+import FavoritesPage from '../pages/favorites-page/favorites-page';
+import LoginPage from '../pages/login-page/login-page';
 import MainPage from '../pages/main-page/main-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
+import PropertyPage from '../pages/property-page/property-page';
 
 type AppPageProps = {
   offersCount: number;
@@ -13,6 +16,15 @@ function App({offersCount}: AppPageProps): JSX.Element {
       <Switch>
         <Route path={AppRoute.Main} exact>
           <MainPage offersCount={offersCount}/>
+        </Route>
+        <Route path={AppRoute.SignIn} exact>
+          <LoginPage/>
+        </Route>
+        <Route path={AppRoute.Favorites} exact>
+          <FavoritesPage/>
+        </Route>
+        <Route path={AppRoute.Room} exact>
+          <PropertyPage/>
         </Route>
         <Route path="">
           <NotFoundPage/>
