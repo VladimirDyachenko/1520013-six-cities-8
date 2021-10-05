a649241911072337225fffbf7d6799e2a237c45f
VladimirDyachenko <d.vovan3@gmail.com>
2021-10-05 15:41:48 +0300

    Создал компоненты описывающие две категории маршрутов
    1) Для приватных private-rote
    2) Для доступных только неавторизированным no-auth-route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AppRoute } from '../../utils/const';
+import { AppRoute, AuthorizationStatus } from '../../utils/const';
 import FavoritesPage from '../pages/favorites-page/favorites-page';
 import LoginPage from '../pages/login-page/login-page';
 import MainPage from '../pages/main-page/main-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
 import PropertyPage from '../pages/property-page/property-page';
+import NoAuthOnlyRoute from '../route-components/no-auth-only-route/no-auth-only-route';
+import PrivateRoute from '../route-components/private-route/private-route';
 
 type AppPageProps = {
   offersCount: number;
@@ -17,15 +19,23 @@ function App({offersCount}: AppPageProps): JSX.Element {
         <Route path={AppRoute.Main} exact>
           <MainPage offersCount={offersCount}/>
         </Route>
-        <Route path={AppRoute.SignIn} exact>
-          <LoginPage/>
-        </Route>
-        <Route path={AppRoute.Favorites} exact>
-          <FavoritesPage/>
-        </Route>
         <Route path={AppRoute.Room} exact>
           <PropertyPage/>
         </Route>
+        <NoAuthOnlyRoute
+          path={AppRoute.SignIn}
+          exact
+          render={() => <LoginPage/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </NoAuthOnlyRoute>
+        <PrivateRoute
+          path={AppRoute.Favorites}
+          exact
+          render={() => <FavoritesPage />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route path="">
           <NotFoundPage/>
         </Route>
--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../utils/const';
+
 function Header(): JSX.Element {
   return (
     <header className='header'>
@@ -11,11 +14,10 @@ function Header(): JSX.Element {
           <nav className='header__nav'>
             <ul className='header__nav-list'>
               <li className='header__nav-item user'>
-                <a className='header__nav-link header__nav-link--profile' href='#temp'>
-                  <div className='header__avatar-wrapper user__avatar-wrapper'>
-                  </div>
+                <Link to={AppRoute.Favorites} className='header__nav-link header__nav-link--profile' href='#temp'>
+                  <div className='header__avatar-wrapper user__avatar-wrapper'></div>
                   <span className='header__user-name user__name'>Oliver.conner@gmail.com</span>
-                </a>
+                </Link>
               </li>
               <li className='header__nav-item'>
                 <a className='header__nav-link' href='#temp'>
