8a30f935c92af6bba2d3bd3553e476f088aa6df2
VladimirDyachenko <d.vovan3@gmail.com>
2021-10-20 00:44:19 +0300

    Доработал компонент Map и создал хук useMap

--- project/src/utils/const.ts
+++ project/src/utils/const.ts
@@ -10,3 +10,15 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const defaultMapIcon = {
+  iconUrl: './img/pin.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39],
+};
+
+export const selectedMapIcon = {
+  iconUrl: './img/pin-active.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39],
+};
