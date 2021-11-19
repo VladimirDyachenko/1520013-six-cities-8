dcdcd66da1180a691d4285ab7864443937c58c72
VladimirDyachenko <d.vovan3@gmail.com>
2021-11-19 22:12:48 +0300

    Ограничил максимальное количество комментариев и отсортировал их по дате

--- project/src/store/property-comments/selectors.ts
+++ project/src/store/property-comments/selectors.ts
@@ -1,5 +1,18 @@
+import { createSelector } from 'reselect';
 import { Comment } from '../../types/comment';
 import { State } from '../../types/store/state';
 import { NameSpace } from '../root-reducer';
 
-export const getPropertyComments = (state: State): Comment[] => state[NameSpace.comments].propertyComments;
+export const getAllPropertyComments = (state: State): Comment[] => state[NameSpace.comments].propertyComments;
+
+const getLimit = (_: State, limit = 10) => limit;
+
+export const getPropertyComments = createSelector(
+  getAllPropertyComments,
+  getLimit,
+  (comments, limit) => (
+    [...comments]
+      .sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+      .slice(0, limit)
+  ),
+);
